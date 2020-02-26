54c0b0d4e86a7cae0a20bd8de353aa724785465a
Kristófer a <andersen19@gmail.com>
2020-02-26 14:00:54 +0100

    Middle table
    This table is for orders and product, with 0 reference between tables

---
+++ OrderApi/Models/OrderLine.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OrderApi.Models
+{
+    public class OrderLine
+    {
+        public int NoOfItems { get; set; }
+    }
+}
