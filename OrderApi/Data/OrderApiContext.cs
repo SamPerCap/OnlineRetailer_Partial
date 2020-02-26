8b65a7baa9b20d74b4e794717a34382c40a553d6
Kristófer a <andersen19@gmail.com>
2020-02-26 13:02:55 +0100

    Added relations in OrderAPI
    Customer has many orders and order has one customer.  Order has 1 product , but product has many orders.

--- OrderApi/Data/OrderApiContext.cs
+++ OrderApi/Data/OrderApiContext.cs
@@ -8,8 +8,24 @@ namespace OrderApi.Data
         public OrderApiContext(DbContextOptions<OrderApiContext> options)
             : base(options)
         {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Order>()
+                 .HasOne(p => p.ProductId)
+                 .WithOne(u => u.Order)
+                 .IsRequired();
+
+            modelBuilder.Entity<Customer>()
+                .HasMany(p => p.Order)
+                .WithOne(u => u.Customer)
+                .IsRequired();
         }
 
         public DbSet<Order> Orders { get; set; }
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Product> Products { get; set; }
     }
 }
